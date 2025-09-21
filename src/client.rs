0a9493eb246584126f6e3abd383ff25db977d423
Justin Moon <mail@justinmoon.com>
2025-09-21 12:26:50 +0100

    server: add closing_result on ClientData and send Closing frame after error responses

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,11 @@
 use std::net::SocketAddr;
 
-use mosaic_core::PublicKey;
+use mosaic_core::{PublicKey, ResultCode};
 
 pub struct ClientData {
     pub remote_address: SocketAddr,
     pub peer: Option<PublicKey>,
     pub mosaic_version: Option<u16>,
     pub applications: Option<Vec<u32>>,
+    pub closing_result: Option<ResultCode>,
 }
